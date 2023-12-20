6cab370d326cc2ba3b29a3f780470ff5ded869ce
jaman-bala <ermekoffdastan@yandex.ru>
2023-12-20 18:21:38 +0600

    edit settings staticfile

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -93,7 +93,7 @@ USE_TZ = True
 # Настройки статических файлов
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Настройки медиафайлов
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
