b96de26c138eebf73ad347df6a9be410add598a0
jaman-bala <ermekoffdastan@yandex.ru>
2023-12-20 15:04:51 +0600

    edit settings staticfile

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -90,11 +90,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Настройки статических файлов
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-# MEDIA
+# Настройки медиафайлов
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
