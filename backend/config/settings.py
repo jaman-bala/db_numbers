bce07ba7a5125af2e577f6069a212d3d9111c520
jaman-bala <ermekoffdastan@yandex.ru>
2024-03-04 14:55:37 +0600

    edit docker-compose

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -92,9 +92,10 @@ USE_TZ = True
 
 # Настройки статических файлов
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 # Настройки медиафайлов
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
